4ff1332f2769e1387f820773ca933d0ee2767c5e
Charlie Verwer <calverwer@gmail.com>
2023-08-31 20:59:41 +0100

    Added Emacs base config

---
+++ .emacs.d/init.el
@@ -0,0 +1,17 @@
+;; Bootstrap emacs config
+(require 'org)
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org-bullets company doom-modeline doom-themes use-package cmake-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
