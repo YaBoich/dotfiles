913d97a77bd6929c32da4ca8545cfb27a7ef79c1
Charlie Verwer <calverwer@gmail.com>
2023-09-22 17:15:54 +0100

    Reworked Org configuration. Moved custom config to its own dir.

--- emacs/init.el
+++ emacs/init.el
@@ -1,17 +1,9 @@
+
 ;; Bootstrap emacs config
 (require 'org)
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(vterm org-bullets company doom-modeline doom-themes use-package cmake-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Load custom Org config (if it exists)
+(let ((org-literate-config-file (expand-file-name "README.org" org-directory)))
+  (when (file-exists-p org-literate-config-file)
+    (org-babel-load-file org-literate-config-file)))
